ace1ba3a85d8e8a7d32000497fa429535fca23e6
chrisjoki <75888059+chrisjoki@users.noreply.github.com>
2021-07-17 18:15:22 +0200

    Update fifo2json.sh

--- fifo2json.sh
+++ fifo2json.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+# MIT License
+#
+# Copyright (c) 2021 chrisjoki
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:#
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # JSON_FILE,StatusCode,PAC,UAC,UDC,Temperature,Timestamp,DeviceId,DAY_ENERGY
 gen_json() 
 {
@@ -22,7 +44,7 @@ gen_json()
 }
  
 if [ "$#" -lt 3 ]; then
- echo "fifo2json.sh fifo json InverterNo BigLog"
+ echo "fifo2json.sh FIFO JSON InverterNo BigLog"
  exit 1
 elif [ "$3" = "1" ]; then
  ENERGY_SUM=/tmp/energy_sum1.txt
